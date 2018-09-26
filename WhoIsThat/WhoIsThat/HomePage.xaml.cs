43afc9d2c9f42fd00d200a32bbaa629d611800af
Dom <domantasgiedraitis@gmail.com>
2018-09-26 15:12:27 +0300

    Added 'taking a photo' functionality and displaying it

--- WhoIsThat/WhoIsThat/HomePage.xaml.cs
+++ WhoIsThat/WhoIsThat/HomePage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +12,7 @@ using Xamarin.Forms.Xaml;
 
 namespace WhoIsThat
 {
+
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HomePage : ContentPage
 	{
@@ -16,5 +20,44 @@ namespace WhoIsThat
 		{
 			InitializeComponent ();
 		}
+
+        private async void takePicture(object sender, EventArgs e)
+        {
+            //Requesting permissions to user camera and storage, too lazy to implement checks if user already have those, later 'todo'
+            await CrossPermissions.Current.RequestPermissionsAsync(Permission.Camera);
+            await CrossPermissions.Current.RequestPermissionsAsync(Permission.Storage);
+
+            //Initializing media hardware
+            await CrossMedia.Current.Initialize();
+
+            //Checking if camera is available, if not -> return
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No camera", "No camera available.", "OK");
+                return;
+            }
+
+            //Taking picture and storing it in default directory which variable file refers to
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    //Directory = "Sample",
+                    //Name = "test.jpg"
+                });
+
+            if (file == null)
+            {
+                return;
+            }
+
+            //Displaying taken photo
+            takenPicture.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
 	}
 }
