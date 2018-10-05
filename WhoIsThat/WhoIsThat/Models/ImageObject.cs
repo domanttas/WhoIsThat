1b99429b7415137e73b75a2a510953352bb1a7c4
Dom <domantasgiedraitis@gmail.com>
2018-10-05 10:50:23 +0300

    Added image object model

---
+++ WhoIsThat/WhoIsThat/Models/ImageObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhoIsThat.Models
+{
+    public class ImageObject
+    {
+        int Id { get; set; }
+
+        string ImageName { get; set; }
+
+        string ImageContentUri { get; set; }
+
+        string PersonFirstName { get; set; }
+
+        string PersonLastName { get; set; }
+    }
+}
