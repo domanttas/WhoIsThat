2900909aa285481ac793256c90d4d58ffafcabb3
Dom <domantasgiedraitis@gmail.com>
2018-10-05 10:28:23 +0300

    Created RestService class for using REST

---
+++ WhoIsThat/WhoIsThat/Connections/RestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WhoIsThat.Connections
+{
+    public class RestService
+    {
+        HttpClient Client { get; set; }
+
+        public RestService()
+        {
+            Client = new HttpClient();
+        }
+    }
+}
