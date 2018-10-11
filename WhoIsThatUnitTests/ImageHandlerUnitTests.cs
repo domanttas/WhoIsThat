9e10b1850d4beeba7db89ada37e7af85774449cc
Dom <domantasgiedraitis@gmail.com>
2018-10-11 14:46:31 +0300

    Added unit test for GetImageFromStream

--- WhoIsThatUnitTests/ImageHandlerUnitTests.cs
+++ WhoIsThatUnitTests/ImageHandlerUnitTests.cs
@@ -2,6 +2,7 @@ using System.IO;
 using NUnit.Framework;
 using Shouldly;
 using WhoIsThat.Handlers;
+using Xamarin.Forms;
 
 namespace WhoIsThatUnitTests
 {
@@ -20,6 +21,22 @@ namespace WhoIsThatUnitTests
             //Assert
             result.ShouldNotBeNull();
             result.ShouldBeOfType(typeof(MemoryStream));
+        }
+
+        [Test]
+        public void GetImageFromStream_ShouldReturn()
+        {
+            //Arrange
+            var uri = "https://whoisthatserverimages.blob.core.windows.net/images/Domantas_test.jpg";
+            var fakeImageHandler = new ImageHandler();
+            var memoryStream = fakeImageHandler.GetStreamFromUri(uri);
+            
+            //Act
+            var result = fakeImageHandler.GetImageFromStream(memoryStream);
+            
+            //Asert
+            result.ShouldNotBeNull();
+            result.ShouldBeOfType(typeof(Image));
         }
     }
 }
