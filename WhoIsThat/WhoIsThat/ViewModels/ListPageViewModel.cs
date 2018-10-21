dce607ddb0f352367e0afce2c81d3396a92f4678
Dom <domantasgiedraitis@gmail.com>
2018-10-21 13:00:34 +0300

    Created ListPage view model

---
+++ WhoIsThat/WhoIsThat/ViewModels/ListPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WhoIsThat.ViewModels
+{
+    public class ListPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
