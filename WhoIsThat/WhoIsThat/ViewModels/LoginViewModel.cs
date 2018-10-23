1439a5182b8190d97b9d56a95530a54416a99b4f
Dom <domantasgiedraitis@gmail.com>
2018-10-23 17:52:22 +0300

    Removed UI from MainPage, left button to sign in with LinkedIn

--- WhoIsThat/WhoIsThat/ViewModels/LoginViewModel.cs
+++ WhoIsThat/WhoIsThat/ViewModels/LoginViewModel.cs
@@ -10,23 +10,16 @@ namespace WhoIsThat.ViewModels
     public class LoginViewModel
     {
         public ICommand NavigateHomePageCommand { get; private set; }
-        public ICommand NavigateRegistrationPageCommand { get; private set; }
         public INavigation Navigation { get; set; }
 
         public LoginViewModel()
         {
             NavigateHomePageCommand = new Command(NavigateToHomePage);
-            NavigateRegistrationPageCommand = new Command(NavigateToRegistrationPage);
         }
 
         public async void NavigateToHomePage()
         {
             await Application.Current.MainPage.Navigation.PushAsync(new HomePage(new HomeViewModel()));
-        }
-
-        public async void NavigateToRegistrationPage()
-        {
-            await Application.Current.MainPage.Navigation.PushAsync(new RegistrationPage(new RegistrationViewModel()));
         }
     }
 }
