c378cf9d506ced97989f997caf2cff8986f07a09
Dom <domantasgiedraitis@gmail.com>
2018-10-23 17:54:21 +0300

    Added new registration page for getting necessary user photo and info after auth

---
+++ WhoIsThat/WhoIsThat/Views/RegisterUserInfo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhoIsThat.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RegisterUserInfo : ContentPage
+	{
+		public RegisterUserInfo ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
