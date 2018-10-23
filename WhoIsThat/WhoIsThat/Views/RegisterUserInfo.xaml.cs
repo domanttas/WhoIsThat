d4452c549956da8a7e783ccd0fd094fe840fd449
Dom <domantasgiedraitis@gmail.com>
2018-10-23 17:57:01 +0300

    Binded registration page to it's view model

--- WhoIsThat/WhoIsThat/Views/RegisterUserInfo.xaml.cs
+++ WhoIsThat/WhoIsThat/Views/RegisterUserInfo.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WhoIsThat.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,13 @@ namespace WhoIsThat.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RegisterUserInfo : ContentPage
 	{
-		public RegisterUserInfo ()
+		public RegisterUserInfo (RegisterUserInfoViewModel registerUserInfoViewModel)
 		{
-			InitializeComponent ();
+            registerUserInfoViewModel.Navigation = Navigation;
+            NavigationPage.SetHasNavigationBar(this, false);
+            BindingContext = registerUserInfoViewModel;
+
+            InitializeComponent ();
 		}
 	}
 }
