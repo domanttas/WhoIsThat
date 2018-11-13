72f7c4d8a34ee2b04bbb993d1756025376613060
Dom <domantasgiedraitis@gmail.com>
2018-11-13 16:39:02 +0200

    Created FaceFeatures model

---
+++ WhoIsThat/WhoIsThat/Models/FaceFeaturesModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhoIsThat.Models
+{
+    public class FaceFeaturesModel
+    {
+        /// <summary>
+        /// Id of user
+        /// </summary>
+        public int PersonId { get; set; }
+
+        /// <summary>
+        /// Age from face
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Gender from face
+        /// </summary>
+        public string Gender { get; set; }
+    }
+}
