a6c7a11786ff6fa9460b1dffb54b57a216ecd572
Dom <domantasgiedraitis@gmail.com>
2018-11-28 09:16:31 +0200

    Participants' page popup with user info

--- WhoIsThat/WhoIsThat/ViewModels/ListPageViewModel.cs
+++ WhoIsThat/WhoIsThat/ViewModels/ListPageViewModel.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
 using WhoIsThat.Handlers;
 using WhoIsThat.Models;
+using WhoIsThat.Views;
+using Xamarin.Forms;
 
 namespace WhoIsThat.ViewModels
 {
@@ -14,6 +19,13 @@ namespace WhoIsThat.ViewModels
 
         public ObservableCollection<ImageObject> People { get; set; }
 
+        public ICommand ParticipantsBackPopUpCommand { get; private set; }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public int Score { get; set; }
+
         public ListPageViewModel(List<ImageObject> list)
         {
             People = new ObservableCollection<ImageObject>();
@@ -22,6 +34,8 @@ namespace WhoIsThat.ViewModels
             {
                 People.Add(person);
             }
+
+            ParticipantsBackPopUpCommand = new Command(ParticipantsBackPopUp);
         }
 
         protected virtual void OnPropertyChanged(string propertyName)
@@ -30,6 +44,29 @@ namespace WhoIsThat.ViewModels
             if (changed != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public async void ItemIsTapped(ImageObject user)
+        {
+            Name = user.PersonFirstName;
+            LastName = user.PersonLastName;
+            Description = user.DescriptiveSentence;
+            Score = user.Score;
+
+            OnPropertyChanged("Name");
+            OnPropertyChanged("LastName");
+            OnPropertyChanged("Description");
+            OnPropertyChanged("Score");
+
+            await PopupNavigation.Instance.PushAsync(new ParticipantsPopUp(this));
+        }
+
+        public async void ParticipantsBackPopUp()
+        {
+            if (PopupNavigation.Instance.PopupStack.Any(p => p is ParticipantsPopUp))
+            {
+                await PopupNavigation.Instance.PopAsync();
             }
         }
     }
--- WhoIsThat/WhoIsThat/Views/ListPage.xaml.cs
+++ WhoIsThat/WhoIsThat/Views/ListPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhoIsThat.Models;
 using WhoIsThat.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,12 +13,25 @@ namespace WhoIsThat.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListPage : ContentPage
 	{
-		public ListPage (ListPageViewModel listPageViewModel)
+        ListPageViewModel ListPageViewModel { get; set; }
+
+		public ListPage (ListPageViewModel viewModel)
 		{
             NavigationPage.SetHasNavigationBar(this, false);
-            BindingContext = listPageViewModel;
+            BindingContext = viewModel;
+
+            ListPageViewModel = viewModel;
 
             InitializeComponent ();
 		}
-	}
+
+        private void ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var list = (ListView)sender;
+            var selected = list.SelectedItem;
+            var index = ListPageViewModel.People.IndexOf((ImageObject)selected);
+
+            ListPageViewModel.ItemIsTapped(ListPageViewModel.People.ElementAt(index));
+        }
+    }
 }
